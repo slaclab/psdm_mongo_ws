6aa19e59ea1e3c8e3bb3159cbd2ec242b62af6fb
Murali Shankar <mshankar@slac.stanford.edu>
2019-11-20 20:12:37 -0800

    Use Python 3 for pyproxy

--- pyproxy.py
+++ pyproxy.py
@@ -17,27 +17,31 @@ import logging
 import argparse
 import shutil
 
-from SocketServer import TCPServer, ForkingTCPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from urllib import urlopen
+import requests
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib.request import urlopen
+
 
 logger = logging.getLogger(__name__)
+remote_url = ""
 
 class ProxyHandler(SimpleHTTPRequestHandler):
-    def __init__(self, request, client_address, server, remote_url):
-        self.remote_url = remote_url
+    def __init__(self, request, client_address, server):
         SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
     def do_GET(self):
-        logger.debug("Path:" + self.path)
-        shutil.copyfileobj(urlopen(self.remote_url + self.path), self.wfile)
+        logger.debug("Path: %s, proxying to %s", self.path, remote_url + self.path)
+        with urlopen(remote_url + self.path) as f:
+            shutil.copyfileobj(f, self.wfile)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='An extremely simplistic HTTP proxy.')
     parser.add_argument('-v', '--verbose', action="store_true")
     parser.add_argument('--interface', default="0.0.0.0", help="The IP address/interface to listen on. For security reasons, listen on an internal IP. This defaults to listening on all interfaces. Hostnames should be acceptable.")
-    parser.add_argument('--port', default=7557, help="The port to listen on.")
+    parser.add_argument('--port', default=7557, help="The port to listen on.", type=int)
     parser.add_argument('remote_url', help="All requests are proxied to this URL. That is, the path of the request is appended to this parameter and used to generate the remote URL from which we will fetch contents.")
     args = parser.parse_args()
+    remote_url = args.remote_url
 
     root = logging.getLogger()
     ch = logging.StreamHandler(sys.stdout)
@@ -54,8 +58,6 @@ if __name__ == '__main__':
     root.addHandler(ch)
 
     server_address = (args.interface, args.port)
-    def createHandler(request, client_address, server):
-        return ProxyHandler(request, client_address, server, args.remote_url)
-    httpd = ForkingTCPServer(server_address, createHandler)
+    httpd = HTTPServer(server_address, ProxyHandler)
     logger.info("Proxying requests to http://{1}:{2}/ to remote URL {0}".format(args.remote_url, args.interface, args.port))
     httpd.serve_forever()
