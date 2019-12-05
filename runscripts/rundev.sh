216d3e7ce17c7994c079b8fd1972b4493d8542b5
Murali Shankar <mshankar@slac.stanford.edu>
2019-12-04 22:57:59 -0700

    Increase the number of workers

--- runscripts/rundev.sh
+++ runscripts/rundev.sh
@@ -23,6 +23,7 @@ fi
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 [ -z "$SERVER_IP_PORT" ] && export SERVER_IP_PORT="0.0.0.0:5000"
 
-exec gunicorn start:app -b ${SERVER_IP_PORT} --worker-class eventlet --reload \
+exec gunicorn start:app -b ${SERVER_IP_PORT} --reload \
        --log-level=DEBUG --env DEBUG=TRUE --capture-output --enable-stdio-inheritance \
+       --worker-class eventlet --workers 4 --max-requests 10000 \
        --access-logfile - --access-logformat "${ACCESS_LOG_FORMAT}"
