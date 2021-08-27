b71fa6a4b9268abd012fa2547e51fbadaac3ea19
Murali Shankar <mshankar@slac.stanford.edu>
2021-08-26 17:58:53 -0700

    Use URL's for the main database as well

--- context.py
+++ context.py
@@ -14,12 +14,10 @@ __author__ = 'mshankar@slac.stanford.edu'
 app = None
 
 # Set up the Mongo connection.
-# For best results, connect using a user account with only read only permissions
-MONGODB_HOST=os.environ['MONGODB_HOST'] or localhost
-MONGODB_PORT=int(os.environ['MONGODB_PORT']) or 27017
-MONGODB_USERNAME=os.environ['MONGODB_USERNAME'] or 'reader'
-MONGODB_PASSWORD=os.environ['MONGODB_PASSWORD'] or 'readerpassword'
-mongoclient = MongoClient(host=MONGODB_HOST, port=MONGODB_PORT, username=MONGODB_USERNAME, password=MONGODB_PASSWORD, authSource="admin", tz_aware=True)
+MONGODB_URL=os.environ.get("MONGODB_URL", None)
+if not MONGODB_URL:
+    print("Please use the enivironment variable MONGODB_URL to configure the calibration database connection.")    
+mongoclient = MongoClient(host=MONGODB_URL, tz_aware=True)
 
 ROLEDB_URL=os.environ.get("ROLEDB_URL", None)
 roledbclient = mongoclient
